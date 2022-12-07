950ed21d063d6ee62276caa004d914c073f4df34
Harry Hammond <harryhammond@Harrys-MacBook-Pro.local>
2022-12-07 17:52:44 +0000

    Aligned the sidebar

---
+++ src/components/Article.js
@@ -0,0 +1,12 @@
+import react from "react";
+import './Article.css'
+
+const Article = () => {
+    return ( 
+        <div className='post'>
+
+        </div>
+     );
+}
+ 
+export default Article;
--- src/components/SideBar.js
+++ src/components/SideBar.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import './SideBar.css'
 
 const SideBar = () => {
     const subReddits = [
