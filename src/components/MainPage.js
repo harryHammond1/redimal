4eb368b3fa74c8262cb418b8e2c339edd0f18cd1
harryHammond1 <harryhmmmnd@gmail.com>
2023-01-09 22:11:53 +0000

    added new logo and made filters horizonal

--- src/components/MainPage.js
+++ src/components/MainPage.js
@@ -1,29 +1,25 @@
-import React from 'react';
-import './MainPage.css'
+import React from "react";
+import "./MainPage.css";
 
 const MainPage = () => {
-    const filters = [
-        { to: '/r/popular', text: 'Popular'},
-        { to: '/r/random', text: 'Random'},
-        { to: '/r/top', text: 'Top'}
-    ]
-    return ( 
-        <div className='mainpage'>
-            <div className='filters'>
-                <ul>
-                    {filters.map(filter => (
-                    <li>
-                        <a href={filter.to}>{filter.text}</a>
-                    </li>)
-                    )}
-                </ul>
-            </div>
-            <div className='posts'>
+  const filters = [
+    { to: "/r/popular", text: "Popular" },
+    { to: "/r/random", text: "Random" },
+    { to: "/r/top", text: "Top" },
+  ];
+  return (
+    <div className="mainpage">
+      <div className="filters">
+        <ul>
+          {filters.map((filter) => (
+            <li>
+              <a href={filter.to}>{filter.text}</a>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
+};
 
-            </div>
-        </div>
-        
-     );
-}
- 
 export default MainPage;
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,21 +1,16 @@
-import React from 'react';
-import './NavBar.css'
-import './redimal.png'
-
+import React from "react";
+import "./NavBar.css";
+import "./redimal.png";
 
 export default function NavBar() {
-    return ( 
-        <div className="NavBar">
-            <div className="logo">
-                <img src={require('./redimal.png') }/>
-            </div>
-            <div className="searchBar">
-                <input type="text" name="search" placeholder='Search the World!' />
-
-            </div>
-        </div>
-
-     );
+  return (
+    <div className="NavBar">
+      <div className="logo">
+        <img className="logo" src={require("./redimal.png")} />
+      </div>
+      <div className="searchBar">
+        <input type="text" name="search" placeholder="Search the World!" />
+      </div>
+    </div>
+  );
 }
- 
-
